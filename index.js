b532461e1a64ac0b490dc5bd2fbb6a8e6f8bd0d5
SantiagoOH21 <soh0621@gmail.com>
2025-05-19 20:49:44 +0200

    tables

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3003;
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "PASSWORD", // AQUÍ MI PASSWORD PARA HACER LAS PRUEBAS
   database: "expressSqlDB",
 });
 
@@ -20,6 +20,26 @@ app.get("/createdb", (req, res) => {
     if (err) throw err;
     console.log(result);
     res.send("Database created...");
+  });
+});
+
+app.get("/createTableCategories", (req, res) => {
+  const sql =
+    "CREATE TABLE categories(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), description TEXT)";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Categories table created...");
+  });
+});
+
+app.get("/createTableProducts", (req, res) => {
+  const sql =
+    "CREATE TABLE posts(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, category_id INT UNSIGNED,name VARCHAR(100), description TEXT, price DECIMAL(10,2), CONSTRAINT category_id_ibfk_1 FOREIGN KEY (category_id) REFERENCES categories(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Products table created...");
   });
 });
 
